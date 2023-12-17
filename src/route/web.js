3786ca62fa7231ec7debdbedb7b3d1d9f7b26460
Star Duong <hvduong2392k4@gmail.com>
2023-12-18 03:15:15 +0700

    four commit

--- src/route/web.js
+++ src/route/web.js
@@ -25,6 +25,9 @@ let initWebRoutes = (app) => {
     router.get('/api/allcode', userController.getAllCode);
 
     router.get('/api/top-doctor-home', doctorController.getTopDoctorHome);
+    router.get('/api/get-all-doctors', doctorController.getAllDoctors);
+    router.post('/api/save-infor-doctors', doctorController.postInforDoctor);
+    router.get('/api/get-detail-doctor-by-id', doctorController.getDetailDoctorById);
     // router.get('/hoidanit', (req, res) => {
     //     return res.send('Hello world with hoidanit')
     // });
