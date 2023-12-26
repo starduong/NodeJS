18dad28700262968cf9559d867c61d19e747a95b
Star Duong <hvduong2392k4@gmail.com>
2023-12-26 10:51:37 +0700

    nine commit

--- src/services/userService.js
+++ src/services/userService.js
@@ -4,9 +4,9 @@ import bcrypt from 'bcryptjs';
 const salt = bcrypt.genSaltSync(10);
 
 let hashUserPassword = (password) => {
-    return new Promise(async (resolve, reject) => {
+    return new Promise((resolve, reject) => {
         try {
-            let hashPassword = await bcrypt.hashSync(password, salt); resolve(hashPassword);
+            let hashPassword = bcrypt.hashSync(password, salt); resolve(hashPassword);
         } catch (e) {
             reject(e);
         }
